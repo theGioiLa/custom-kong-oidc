54530437b0bcc3e80ecb698857cc0b4faa63e826
theGioiLa <quangquyK60@gmail.com>
2020-10-29 04:46:52 +0630

    Indentation refactoring

--- filter.lua
+++ filter.lua
@@ -1,17 +1,17 @@
 local M = {}
 
 local function shouldIgnoreRequest(patterns)
-  if (patterns) then
-    for _, pattern in ipairs(patterns) do
-      local isMatching = not (string.find(ngx.var.uri, pattern) == nil)
-      if (isMatching) then return true end
+    if (patterns) then
+        for _, pattern in ipairs(patterns) do
+            local isMatching = not (string.find(ngx.var.uri, pattern) == nil)
+            if (isMatching) then return true end
+        end
     end
-  end
-  return false
+    return false
 end
 
 function M.shouldProcessRequest(config)
-  return not shouldIgnoreRequest(config.filters)
+    return not shouldIgnoreRequest(config.filters)
 end
 
 return M
--- handler.lua
+++ handler.lua
@@ -8,46 +8,46 @@ OidcHandler.PRIORITY = 1000
 
 
 function OidcHandler:new()
-  OidcHandler.super.new(self, "oidc")
+    OidcHandler.super.new(self, "oidc")
 end
 
 function OidcHandler:access(config)
-  OidcHandler.super.access(self)
-  local oidcConfig = utils.get_options(config, ngx)
+    OidcHandler.super.access(self)
+    local oidcConfig = utils.get_options(config, ngx)
 
-  if filter.shouldProcessRequest(oidcConfig) then
-    session.configure(config)
-    handle(oidcConfig)
-  else
-    ngx.log(ngx.DEBUG, "OidcHandler ignoring request, path: " .. ngx.var.request_uri)
-  end
+    if filter.shouldProcessRequest(oidcConfig) then
+        session.configure(config)
+        handle(oidcConfig)
+    else
+        ngx.log(ngx.DEBUG, "OidcHandler ignoring request, path: " .. ngx.var.request_uri)
+    end
 
-  ngx.log(ngx.DEBUG, "OidcHandler done")
+    ngx.log(ngx.DEBUG, "OidcHandler done")
 end
 
 function handle(oidcConfig)
-  local response
-  if oidcConfig.introspection_endpoint then
-    response = introspect(oidcConfig)
-    if response then
-      utils.injectUser(response)
+    local response
+    if oidcConfig.introspection_endpoint then
+        response = introspect(oidcConfig)
+        if response then
+            utils.injectUser(response)
+        end
     end
-  end
 
-  if response == nil then
-    response = make_oidc(oidcConfig)
-    if response then
-      if (response.user) then
-        utils.injectUser(response.user)
-      end
-      -- if (response.access_token) then
-        -- utils.injectAccessToken(response.access_token)
-      -- end
-      -- if (response.id_token) then
-        -- utils.injectIDToken(response.id_token)
-      -- end
+    if response == nil then
+        response = make_oidc(oidcConfig)
+        if response then
+            if (response.user) then
+                utils.injectUser(response.user)
+            end
+            -- if (response.access_token) then
+                -- utils.injectAccessToken(response.access_token)
+            -- end
+            -- if (response.id_token) then
+                -- utils.injectIDToken(response.id_token)
+            -- end
+        end
     end
-  end
 
 	if (response) then
 		local user = response
@@ -76,33 +76,32 @@ function authZ_by_roles(u_roles, match_roles)
 end
 
 function make_oidc(oidcConfig)
-  ngx.log(ngx.DEBUG, "OidcHandler calling authenticate, requested path: " .. ngx.var.request_uri)
-  local res, err = require("resty.openidc").authenticate(oidcConfig)
-  if err then
-    if oidcConfig.recovery_page_path then
-      ngx.log(ngx.DEBUG, "Entering recovery page: " .. oidcConfig.recovery_page_path)
-      ngx.redirect(oidcConfig.recovery_page_path)
+    ngx.log(ngx.DEBUG, "OidcHandler calling authenticate, requested path: " .. ngx.var.request_uri)
+    local res, err = require("resty.openidc").authenticate(oidcConfig)
+    if err then
+        if oidcConfig.recovery_page_path then
+            ngx.log(ngx.DEBUG, "Entering recovery page: " .. oidcConfig.recovery_page_path)
+            ngx.redirect(oidcConfig.recovery_page_path)
+        end
+        utils.exit(500, err, ngx.HTTP_INTERNAL_SERVER_ERROR)
     end
-    utils.exit(500, err, ngx.HTTP_INTERNAL_SERVER_ERROR)
-  end
-  return res
+    return res
 end
 
 function introspect(oidcConfig)
-  if utils.has_bearer_access_token() or oidcConfig.bearer_only == "yes" then
-    local res, err = require("resty.openidc").introspect(oidcConfig)
-    if err then
-      if oidcConfig.bearer_only == "yes" then
-        ngx.header["WWW-Authenticate"] = 'Bearer realm="' .. oidcConfig.realm .. '",error="' .. err .. '"'
-        utils.exit(ngx.HTTP_UNAUTHORIZED, err, ngx.HTTP_UNAUTHORIZED)
-      end
-      return nil
+    if utils.has_bearer_access_token() or oidcConfig.bearer_only == "yes" then
+        local res, err = require("resty.openidc").introspect(oidcConfig)
+        if err then
+            if oidcConfig.bearer_only == "yes" then
+                ngx.header["WWW-Authenticate"] = 'Bearer realm="' .. oidcConfig.realm .. '",error="' .. err .. '"'
+                utils.exit(ngx.HTTP_UNAUTHORIZED, err, ngx.HTTP_UNAUTHORIZED)
+            end
+            return nil
+        end
+        ngx.log(ngx.DEBUG, "OidcHandler introspect succeeded, requested path: " .. ngx.var.request_uri)
+        return res
     end
-    ngx.log(ngx.DEBUG, "OidcHandler introspect succeeded, requested path: " .. ngx.var.request_uri)
-    return res
-  end
-  return nil
+    return nil
 end
-
 
 return OidcHandler
